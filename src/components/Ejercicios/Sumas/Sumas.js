b503a72852487be9d1bed54ec6c6e1a4b20ad1f4
Estela Paco <epaco@ar.ibm.com>
2020-06-23 20:00:23 -0300

    se modifica el diseño de juego sumas

--- src/components/Ejercicios/Sumas/Sumas.js
+++ src/components/Ejercicios/Sumas/Sumas.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { withStyles  } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Api from '../../../controller/Api';
 import Modal from '@material-ui/core/Modal';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-
+import Container from '@material-ui/core/Container';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 // const Container = styled.div`
 //   max-width: 980px;
 //   margin: 0 auto;
@@ -25,29 +27,6 @@ const useStyles = theme => ({
     width: '100%',
     maxWidth: 500,
   },
-  option:{
-    border: 'solid',
-    // // border-radius: '20px',
-    // cursor: 'pointer',
-    // padding: '25px',
-    // bgcolor: 'background.paper',
-    // borderColor: 'text.primary',
-    // m: 1,
-    // border: 100,
-    // style: { width: '5rem', height: '5rem' },
-    marginRight: 15,
-    // marginTop: 0,
-    backgroundColor: '#FFFFFF',
-    // color: '#000000',
-    borderRadius: '10px',
-    // display: 'inline-block',
-    fontSize: '25px',
-    height: '70px',
-    width: '70px',
-    // border: '0',
-    textAlign: 'center',
-    // transition: 'all 290ms cubic-bezier(0.79, 0.01, 0.38, 0.99)',
-  },
   boton: {
     marginRight: 16,
     marginTop: 5,
@@ -62,6 +41,25 @@ const useStyles = theme => ({
     textAlign: 'center',
     transition: 'all 290ms cubic-bezier(0.79, 0.01, 0.38, 0.99)',
   },
+  option:{
+    border: 'solid',
+    marginRight: 15,
+    backgroundColor: '#FFFFFF',
+    borderRadius: '25px',
+    fontSize: '25px',
+    height: '70px',
+    width: '70px',
+    textAlign: 'center',
+  },
+  boxText: {
+    border: 'solid',
+    marginRight: 25,
+    backgroundColor: '#FFC226',
+    borderRadius: '10px',
+    height: '75px',
+    width: '90px',
+    
+  },
   paper: {
     position: 'absolute',
     backgroundColor: theme.palette.background.paper,
@@ -69,18 +67,15 @@ const useStyles = theme => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
-  labelRoot: {
+  title: {
     fontSize: 30,
-    color: "green",
-    "&$labelFocused": {
-      color: "blue",
-      fontSize: 20,
-    }
+    color: "write",
+    textAlign: 'center',
   },
-  labelFocused: {},
-  input: {
-    fontSize: '32px',
-    color: '#FFFFFF',
+  titleOperation: {
+    fontSize: 30,
+    color: "write",
+    textAlign: 'center',
   },
   notchedOutline: {
     borderWidth: "2px",
@@ -119,6 +114,7 @@ class Sumas extends Component {
       setOpenError: '',
       resultados: [],
       puntos: 0,
+      valueNext: 'CINCO',
     };
   }
   handleClick = () => {
@@ -158,6 +154,7 @@ class Sumas extends Component {
     console.log(this.state.resultados);
     this.setState({resultados: []});
     Api.obtenerNivelSumas(this.state.nivel + 1, this.resultSumas.bind(this));
+    this.setState({valueNext: 'CINCO'});
   }
 
   guardarResultado(operacion, e) {
@@ -167,92 +164,86 @@ class Sumas extends Component {
     this.setState({resultados: array});
   }
 
-  addToAnswer = (item, resultado) => {
+  addToAnswer = (item, row) => {
     var result = this.state.puntos;
-    
-    if(item === resultado){
-      this.setState({open: true})
-      this.setState({openError: false})
+    if(item === row.resultado){
+      this.setState({open: true});
+      this.setState({openError: false});
       result = result + 5;
-      this.setState({puntos: result})
+      this.setState({puntos: result});
     } else {
       this.setState({openError: true})
       this.setState({open: false})
       result = result - 5;
       this.setState({puntos: result})
+    } 
+    row.value = 'pulso';
+    var select = this.state.valueNext.substr(1);
+    this.setState({valueNext: select});
+    if (select.charAt(0) === ''){
+      this.setState({valueNext: ''});
     }
-    
   }
 
   render() {
     const { classes } = this.props;
-    
+console.log(this.props);
     return (
-      <div className='coumn'>
-        <h1 className='title'>Hola {this.props.name}</h1>
-        <h2 className='titleCenter'>Practiquemos las Sumas</h2>
-        <h3 className='titleCenter'>Nivel {this.state.nivel}</h3>
-
-
-        {this.state.nivel === 1 && <form className={classes.root}>
-          {this.state.data1.map((row) => ( 
-            <div className='row center'>
-              <div className={classes.rootA} id={row.id}>
-                <Typography variant="h3" component="h2" gutterBottom>
-                  {row.operacion}
-                </Typography>
+      <React.Fragment>
+          <Container>           
+           <CardActions>
+            <Typography className={classes.title} variant="h3">
+            <Button><Typography className={classes.title} variant="h3">{this.props.name}</Typography></Button>
+             , practiquemos las Sumas!!!
+            </Typography>
+           </CardActions>
+            <h3 className='titleCenter'>Nivel {this.state.nivel}</h3>
+            <Container> 
+              {this.state.nivel === 1 && <form className={classes.root}>
+              {this.state.data1.map((row) => ( 
+              <div className='row center'>
+                <Grid> 
+                  <Button className={classes.boxText} variant="contained" color="default" disableElevation id={row.id}>{row.operacion}</Button>        
+                </Grid>
+                {
+                row.opciones.map((item) => 
+                  <Button disabled={row.value} className={classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row)}>{item}</Button>        
+                )}
               </div>
-              {
-              row.opciones.map((item) => 
-                // <span className={`title-medium m-right-5`, classes.option} id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</span>,
-                <div>
-                  <Button className={`title-medium m-right-5`, classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</Button>
-                </div>             
-                )
-              }
-            </div>
-          ))}
-        </form>}
-        {this.state.nivel === 2 && <form className={classes.root}>
-          {this.state.data2.map((row) => (        
+              ))}
+              </form>}
+            </Container> 
+            <Container> 
+              {this.state.nivel === 2 && <form className={classes.root}>
+              {this.state.data2.map((row) => ( 
               <div className='row center'>
-              <div className={classes.rootA} id={row.id}>
-              <Typography variant="h3" component="h2" gutterBottom>
-                  {row.operacion}
-              </Typography>
-            </div>
-            {
-              row.opciones.map((item) => 
-              // <span className={`title-medium m-right-5`, classes.option} id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</span>,
-              <div>
-                <Button className={`title-medium m-right-5`, classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</Button>
-              </div> 
-              )
-            }
-            </div>
-            ))}
-        </form>}
-        {this.state.nivel === 3 && <form className={classes.root}>
-          {this.state.data3.map((row) => (        
+                <Grid> 
+                  <Button className={classes.boxText} variant="contained" color="default" disableElevation id={row.id}>{row.operacion}</Button>        
+                </Grid>
+                {
+                row.opciones.map((item) => 
+                  <Button disabled={row.value} className={classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row)}>{item}</Button>        
+                )}
+              </div>
+              ))}
+              </form>}
+            </Container>
+            <Container> 
+              {this.state.nivel === 3 && <form className={classes.root}>
+              {this.state.data3.map((row) => ( 
               <div className='row center'>
-              <div className={classes.rootA} id={row.id}>
-              <Typography variant="h3" component="h2" gutterBottom>
-                  {row.operacion}
-              </Typography>
-            </div>
-            {
-              row.opciones.map((item) => 
-              // <span className={`title-medium m-right-5`, classes.option} id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</span>,
-              <div>
-                <Button className={`title-medium m-right-5`, classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row.resultado)}>{item}</Button>
-              </div> 
-              )
-            }
-            </div>
-            ))}
-        </form>}
-
-        <div>
+                <Grid> 
+                  <Button className={classes.boxText} variant="contained" color="default" disableElevation id={row.id}>{row.operacion}</Button>        
+                </Grid>
+                {
+                row.opciones.map((item) => 
+                  <Button disabled={row.value} className={classes.option} variant="contained" color="default" disableElevation id={row.id} onClick={() => this.addToAnswer(item, row)}>{item}</Button>        
+                )}
+              </div>
+              ))}
+              </form>}
+            </Container>
+            <div>
               <Snackbar open={this.state.open} autoHideDuration={1500} onClose={this.handleClose}>
                 <Alert onClose={this.handleClose} severity="success">
                   Correcto! SUMAS PUNTOS  +5 !!
@@ -263,33 +254,30 @@ class Sumas extends Component {
                   Incorrecto! RESTAS PUNTOS  -5 !!
                 </Alert>
               </Snackbar>
-        </div>
-        <div>
-          <Button className={classes.boton} variant="contained" component={Link} to={'/seleccion'}>Salir</Button>
-          {/* No tiene sentido retrocederNivel ? */}
-          {/* {this.state.nivel > 1 && <Button variant="contained" color="primary" onClick={this.retrocederNivel.bind(this)} className={classes.boton}>Anterior</Button>} */}
-          {this.state.nivel < 3 && <Button variant="contained" color="primary" onClick={this.avanzarNivel.bind(this)} className={classes.boton}>Siguiente</Button>}
-          {this.state.nivel === 3 && <Button variant="contained" color="primary" onClick={() => this.setState({openModal: true})} className={classes.boton}>Finalizar</Button>}
-        </div>
-
-
-
-
-        <Modal
-        open={this.state.openModal}
-        onClose={this.handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        >
-          <div style={getModalStyle()} className={classes.paper}>
-            <h2 id="simple-modal-title">Terminaste los ejercicios</h2>
-            <p id="simple-modal-description">
-              Tu puntaje es {this.state.puntos}
-            </p>
-            <Button variant="contained" onClick={this.handleClose.bind(this)}>Aceptar</Button>
-          </div>
-        </Modal>
-      </div>
+            </div>
+            <div className='row center'>
+              <Button className={classes.boton} variant="contained" component={Link} to={'/seleccion'}>Salir</Button>
+              {/* No tiene sentido retrocederNivel ? */}
+              {/* {this.state.nivel > 1 && <Button variant="contained" color="primary" onClick={this.retrocederNivel.bind(this)} className={classes.boton}>Anterior</Button>} */}
+              {this.state.nivel < 3 && <Button variant="contained" color="primary" onClick={this.avanzarNivel.bind(this)} disabled={this.state.valueNext} className={classes.boton}>Siguiente</Button>}
+              {this.state.nivel === 3 && <Button variant="contained" color="primary" onClick={() => this.setState({openModal: true})} disabled={this.state.valueNext} className={classes.boton}>Finalizar</Button>}
+            </div>
+            <Modal
+            open={this.state.openModal}
+            onClose={this.handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            >
+              <div style={getModalStyle()} className={classes.paper}>
+                <h2 id="simple-modal-title">Terminaste los ejercicios</h2>
+                <p id="simple-modal-description">
+                  Tu puntaje es {this.state.puntos}
+                </p>
+                <Button variant="contained" onClick={this.handleClose.bind(this)}>Aceptar</Button>
+              </div>
+            </Modal>
+          </Container>
+      </React.Fragment>
     );
   }
 }
